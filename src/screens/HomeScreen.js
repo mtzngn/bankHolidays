bf630e1d1b295438f2423c4cbdac7c432a6c0dd5
Taha <tzengin@yozu.co.uk>
2022-06-30 19:07:07 +0100

    addDivisions and filterBySixMonthFromNow functions implemented

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,13 +4,16 @@ import {fetchBankHolidays} from '../api';
 import {useDispatch} from 'react-redux';
 import {initilizeBankHolidays} from '../reducers/bankHolidaysReducer';
 import EventList from '../components/EventList/EventList';
+import {addDivisons, filterBySixMonthFromNow} from '../utils';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
 
   const initiliazeReduxStore = async () => {
     const bankHolidays = await fetchBankHolidays();
-    const events = bankHolidays['england-and-wales'].events;
+    const unFilteredEvents = addDivisons(bankHolidays);
+    const events = filterBySixMonthFromNow(unFilteredEvents).slice(0, 5);
+
     dispatch(
       initilizeBankHolidays({
         events,
---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+export const filterBySixMonthFromNow = eventsArray => {
+  const sixMonthInFuture = new Date();
+  sixMonthInFuture.setMonth(sixMonthInFuture.getMonth() + 6);
+
+  return eventsArray.filter(
+    ({date}) =>
+      sixMonthInFuture > new Date(date) && new Date(date) > new Date(),
+  );
+};
+
+export const addDivisons = eventsArray => {
+  const englandDays = eventsArray['england-and-wales'].events.map(el => {
+    return {...el, division: eventsArray['england-and-wales'].division};
+  });
+  const scotlandDays = eventsArray.scotland.events.map(el => {
+    return {...el, division: eventsArray.scotland.division};
+  });
+  const northenIrelandDays = eventsArray['northern-ireland'].events.map(el => {
+    return {...el, division: eventsArray['northern-ireland'].division};
+  });
+  return [...englandDays, ...scotlandDays, ...northenIrelandDays];
+};
