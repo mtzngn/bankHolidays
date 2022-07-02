7604711eaa3f3e2f65c67888dc37005d9fd917e9
Taha <tzengin@yozu.co.uk>
2022-07-02 14:42:23 +0100

    test added for CustomInput

---
+++ src/common/__test__/CustomInput.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import CustomInput from '../CustomInput';
+
+const mockOnChangeText = jest.fn();
+const props = {
+  value: 'testValue',
+  onChangeText: mockOnChangeText,
+};
+
+describe('CustomInput', () => {
+  describe('renders', () => {
+    it('should render', () => {
+      const {getByDisplayValue} = render(<CustomInput {...props} />);
+      expect(getByDisplayValue('testValue')).toBeTruthy();
+    });
+  });
+  describe('OnChange', () => {
+    it('should call onChangeText', () => {
+      const {getByDisplayValue} = render(<CustomInput {...props} />);
+      fireEvent.changeText(getByDisplayValue('testValue'), 'newValue');
+      expect(mockOnChangeText).toHaveBeenCalled();
+    });
+  });
+});
